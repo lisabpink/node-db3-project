bd5fba65888b984305ae663361cfaa6095d4158f
LISA_COOK <lisabpink@gmail.com>
2020-03-18 18:07:10 -0400

    moved config file- server broken

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -1,29 +1,55 @@
-const db = require('../data/config')
+const db = require("../data/seeds/config");
+
+module.exports = {
+  find,
+  findById,
+  add,
+  update,
+  remove,
+  findSteps,
+  addStep
+};
 
 function find() {
-
+  return db("schemes");
 }
 
 function findById(id) {
-
+  return db("schemes")
+    .where("id", id)
+    .first();
 }
 
-function add(scheme){
-
+function findSteps(id) {
+  return db("schemes as s")
+    .where({ scheme_id: id })
+    .join("steps as st", "s.id", "st.scheme_id")
+    .select("s.scheme_name", "st.step_number", "st.instructions")
+    .where({ "s.id": id })
+    .orderBy("step_number", "asc");
 }
 
-function update(changes, id){
-
+function add(schemeData) {
+  return db("schemes")
+    .insert(schemeData)
+    .then(ids => {
+      return findById(ids[0]);
+    });
 }
 
-function remove(id){
-
+function addStep({ instructions, step_number }, scheme_id) {
+  return db("steps")
+  .insert({ instructions, step_number, scheme_id });
 }
 
-module.exports = {
-    find, 
-    findById,
-    add,
-    update,
-    remove
+function update(changes, id) {
+  return db("schemes")
+    .where({ id: id })
+    .update(changes);
+}
+
+function remove(id) {
+  return db("schemes")
+    .where({ id: id })
+    .del();
 }
